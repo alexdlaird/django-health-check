b534c8d932ac66df14bad735ab7fa8593a382c54
Alexander Laird <alaird@twilio.com>
2018-04-25 16:14:27 -0700

    Finished implementing Twilio health check.

--- health_check/contrib/twilio/backends.py
+++ health_check/contrib/twilio/backends.py
@@ -1,4 +1,8 @@
+import json
+import urllib.request
+
 from health_check.backends import BaseHealthCheckBackend
+from health_check.exceptions import ServiceWarning
 
 
 class TwilioBackendHealthCheck(BaseHealthCheckBackend):
@@ -8,8 +12,14 @@ class TwilioBackendHealthCheck(BaseHealthCheckBackend):
 
     def check_status(self):
         if len(self.services) > 0:
-            'https://gpkpyklzq55q.statuspage.io/api/v2/status.json'
-            pass
+            response = urllib.request.urlopen('https://gpkpyklzq55q.statuspage.io/api/v2/summary.json')
+            data = json.loads(response.read().decode('utf-8'))
+            for component in data['components']:
+                if component['name'] in self.services and component['status'] != 'operational':
+                    self.add_error(ServiceWarning(
+                        'Twilio service {} returned status {}'.format(component['name'], data['status'])))
         else:
-            'https://gpkpyklzq55q.statuspage.io/api/v2/summary.json'
-            pass
+            response = urllib.request.urlopen('https://gpkpyklzq55q.statuspage.io/api/v2/status.json')
+            data = json.loads(response.read().decode('utf-8'))
+            if data['status']['indicator'] != 'none':
+                self.add_error(ServiceWarning(data['status']['indicator']))
