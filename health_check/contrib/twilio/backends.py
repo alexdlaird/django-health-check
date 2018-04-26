c44c5afd3cf7415303b98dd353ace26a973e4b88
Alex Laird <alexdlaird@gmail.com>
2018-04-25 22:08:46 -0700

    Adding timeout for Twilio status check.

--- health_check/contrib/twilio/backends.py
+++ health_check/contrib/twilio/backends.py
@@ -1,6 +1,8 @@
 import json
 import urllib.request
 
+from django.conf import settings
+
 from health_check.backends import BaseHealthCheckBackend
 from health_check.exceptions import ServiceWarning
 
@@ -11,15 +13,17 @@ class TwilioBackend(BaseHealthCheckBackend):
         self.services = getattr(self, 'services', [])
 
     def check_status(self):
+        timeout = getattr(settings, 'HEALTHCHECK_TWILIO_TIMEOUT', 3)
+
         if len(self.services) > 0:
-            response = urllib.request.urlopen('https://gpkpyklzq55q.statuspage.io/api/v2/summary.json')
+            response = urllib.request.urlopen('https://gpkpyklzq55q.statuspage.io/api/v2/summary.json', timeout=timeout)
             data = json.loads(response.read().decode('utf-8'))
             for component in data['components']:
                 if component['name'] in self.services and component['status'] != 'operational':
                     self.add_error(ServiceWarning(
                         '{} - {}'.format(component['name'], component['status'])))
         else:
-            response = urllib.request.urlopen('https://gpkpyklzq55q.statuspage.io/api/v2/status.json')
+            response = urllib.request.urlopen('https://gpkpyklzq55q.statuspage.io/api/v2/status.json', timeout=timeout)
             data = json.loads(response.read().decode('utf-8'))
             if data['status']['indicator'] != 'none':
                 self.add_error(ServiceWarning(data['status']['indicator']))
