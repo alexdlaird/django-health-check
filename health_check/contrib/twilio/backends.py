1e5de31930694869f56e5d8b0e1d816544682db5
Alexander Laird <alaird@twilio.com>
2018-04-27 11:15:45 -0700

    Wrapping Twilio failure in exception.

--- health_check/contrib/twilio/backends.py
+++ health_check/contrib/twilio/backends.py
@@ -4,7 +4,7 @@ import urllib.request
 from django.conf import settings
 
 from health_check.backends import BaseHealthCheckBackend
-from health_check.exceptions import ServiceWarning
+from health_check.exceptions import ServiceWarning, ServiceUnavailable
 
 
 class TwilioHealthCheck(BaseHealthCheckBackend):
@@ -13,19 +13,22 @@ class TwilioHealthCheck(BaseHealthCheckBackend):
         self.services = getattr(self, 'services', [])
 
     def check_status(self):
-        timeout = getattr(settings, 'HEALTHCHECK_TWILIO_TIMEOUT', 3)
+        timeout = getattr(settings, 'HEALTHCHECK_TWILIO_TIMEOUT', 5)
 
-        if len(self.services) > 0:
-            response = urllib.request.urlopen('https://gpkpyklzq55q.statuspage.io/api/v2/summary.json',
-                                              timeout=timeout)
-            data = json.loads(response.read().decode('utf-8'))
-            for component in data['components']:
-                if component['name'] in self.services and component['status'] != 'operational':
-                    self.add_error(ServiceWarning(
-                        '{} - {}'.format(component['name'], component['status'])))
-        else:
-            response = urllib.request.urlopen('https://gpkpyklzq55q.statuspage.io/api/v2/status.json',
-                                              timeout=timeout)
-            data = json.loads(response.read().decode('utf-8'))
-            if data['status']['indicator'] != 'none':
-                self.add_error(ServiceWarning(data['status']['indicator']))
+        try:
+            if len(self.services) > 0:
+                response = urllib.request.urlopen('https://gpkpyklzq55q.statuspage.io/api/v2/summary.json',
+                                                  timeout=timeout)
+                data = json.loads(response.read().decode('utf-8'))
+                for component in data['components']:
+                    if component['name'] in self.services and component['status'] != 'operational':
+                        self.add_error(ServiceWarning(
+                            '{} - {}'.format(component['name'], component['status'])))
+            else:
+                response = urllib.request.urlopen('https://gpkpyklzq55q.statuspage.io/api/v2/status.json',
+                                                  timeout=timeout)
+                data = json.loads(response.read().decode('utf-8'))
+                if data['status']['indicator'] != 'none':
+                    self.add_error(ServiceWarning(data['status']['indicator']))
+        except BaseException as e:
+            self.add_error(ServiceUnavailable("Unknown error"), e)
